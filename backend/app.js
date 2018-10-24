0fcbc09a39acd59a42fcc2b725672088740d9dab
Pham Van Tai <phamvantaiqn93@gmail.com>
2018-10-24 17:34:19 +0700

    Lesson 40: Adding the POST backend Point

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use('/api/posts', (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log("post /api/posts");
+  res.status(201).json({
+    message: 'Post added sucessfully'
+  });
+});
+app.get('/api/posts', (req, res, next) => {
   console.log('first middleware');
   const posts = [
     {
@@ -18,7 +39,7 @@ app.use('/api/posts', (req, res, next) => {
 
   ];
   res.status(200).json({
-    message: 'Posts fetched succesfully!',
+    message: 'Posts fetched sucessfully!',
     posts: posts
   });
 });
