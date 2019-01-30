75043bf50b308e6543d20e92bde0e63ac98cf172
Pham Van Tai <phamvantaiqn93@gmail.com>
2019-01-30 17:46:28 +0700

    lesson 102

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
@@ -25,4 +27,35 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+router.post("/login", (req, res, next) => {
+  User.find({email: req.body.email})
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      return bcrypt.compare(req.body.password, user.password)
+    })
+    .then(result => {
+      console.log(result);
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        })
+      }
+      const token = jwt.sign({email: user.email, userId: user._id},
+        "secret_this_should_be_longer", { expiresIn: "1h"});
+      res.status(200).json({
+        token: token
+      })
+      console.log(token);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(401).json({
+        message: "Auth failed"
+      })
+    })
+});
 module.exports = router;
